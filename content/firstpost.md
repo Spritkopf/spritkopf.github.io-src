aea24e36c23ac6d54812d6323744e285565d9f3e
Ricardo Beck <rbck@office.amsiag.com>
2017-08-18 11:34:11 +0200

    First commit of pelican base

---
+++ content/firstpost.md
@@ -0,0 +1,23 @@
+Title: First test post
+Date: 2010-12-03 10:20
+Modified: 2010-12-05 19:30
+Category: Python
+Tags: pelican, publishing
+Slug: first-test-post
+Authors: Ricardo
+Summary: Short version for index and feeds
+Headline: this is pelican
+
+This is the content of the first blog post. pleasy stand by.
+
+here is some code
+
+	:::python
+	print("hello world")
+
+with line numbers:
+
+	#!python
+	import serial
+
+	print('hey')
---
+++ pelicanconf.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Ricardo'
+SITENAME = 'rb_dev'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+LOCALE = ('usa',  # On Windows
+    'en_US',     # On Unix/Linux
+    )
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+#LINKS = (('sdadsf','sdasdf'))
+
+GITHUB_URL = 'http://github.com/spritkopf'
+
+# Social widget
+SOCIAL = (('GitHub', GITHUB_URL),
+          )
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+# Specify name of a built-in theme
+THEME = "pelican-clean-blog"
+
+
+MENUITEMS = (('Archives', '/archives.html'),
+             ('Categories', '/categories.html'),
+             ('Tags', '/tags.html'),)
+
+
+
+SITESUBTITLE = "Subtitle of the site...."
+
+COLOR_SCHEME_CSS = 'monokai.css'
